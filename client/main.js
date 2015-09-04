32f51b75d234cefd97a42750448403fd6441fcb8
Benjamin Norrington <ben@norrington.net>
2015-09-04 11:09:00 +0100

    fixed bugs introduced by nick :)

--- client/main.js
+++ client/main.js
@@ -10,35 +10,34 @@ document.addEventListener('DOMContentLoaded', function () {
     var Selection = Slate.Selection;
 	var Region = Slate.Region;
 	var e = Slate.editor;
-	var sexprDoc;
 
 	if(docMode=='edit'){
 		var dummy = new BCSocket(null, {reconnect: true});
 		dummy.canSendJSON = false; //need this because goog.json.serialize doesn't call toJSON	
 	} else {
-		var dummy = new Slate.Dummy(Slate.ottypes);		
-		sexprDoc = { 
-			data: docSexpr, //docSexpr is set in the view
-			type: 'sexpr',
-			v: 0,
-		};		
+		var dummy = new Slate.Dummy(Slate.ottypes);
 	}
 
 	var sjs = new Slate.sharejs.Connection(dummy);
 	window.share_connection = sjs;
 	sjs.debug = true;
-	var sharedoc = sjs.get(docCollection, docId, sexprDoc); //docCollection and docId set in the view		
+	var sharedoc = sjs.get(docCollection, docId); //docCollection and docId set in the view
 	sharedoc.subscribe();
 
 	var editor;
 	var store;
 
+	var doc = window.docSexpr || '(doc (section (h1 "") (p "")))';
+
 	var sel = new Selection([new Region(7,7)]);	
 
 	var catalog = window.catalog || 'unknown';
 
-	sharedoc.whenReady(function() {		
-		sharedoc.snapshot = Slate.type.deserialize(sharedoc.snapshot);
+	sharedoc.whenReady(function() {
+		if (!sharedoc.type)
+			sharedoc.create('sexpr', doc);
+		else
+			sharedoc.snapshot = Slate.type.deserialize(sharedoc.snapshot);
 		store = new Slate.Store(sharedoc.createContext(), Slate.type);
 
 		store.select(sel);
